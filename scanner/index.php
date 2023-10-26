a7f1869c9536321245eb54aa704842ec4d11e6c6
JeromeBern <jeromebernante@gmail.com>
2023-10-26 21:46:20 +0800

    scanner test

---
+++ scanner/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>QR/Barcode Scanner</title>
+</head>
+<body>
+    <video id="video" width="100%"></video>
+    <button id="scan-btn">Scan</button>
+    <div id="result"></div>
+    
+    <script src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/qr_packed.js"></script>
+    <script src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/webqr.js"></script>
+    <script src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/llqrcode.js"></script>
+    <script src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/plusone.js"></script>
+    <script src="https://rawgit.com/sitepoint-editors/jsqrcode/master/src/grid.js"></script>
+    
+    <script src="scanner.js"></script>
+</body>
+</html>
